ac9c78eb740c91184614267dc7bdecf897e06191
NewNow974 <49619618+NewNow974@users.noreply.github.com>
2020-04-04 23:10:38 +0200

    Eloi2

--- graphe.py
+++ graphe.py
@@ -1,3 +1,4 @@
+
 from lien import Lien
 from noeud import Noeud
 
@@ -6,15 +7,16 @@ class Graphe:
     """Class Graphe"""
 
     """Initialize the value."""
+
     def __init__(self, identifiantGraphe):
-        self._identifiantGraphe=identifiantGraphe
-        self._numberNoeuds=0
+        self._identifiantGraphe = identifiantGraphe
+        self._numberNoeuds = 0
 
-        self._dic_Noeuds={}
-        self._dic_Liens={}
-
+        self._dic_Noeuds = {}
+        self._dic_Liens = {}
 
     """Getters et Setterse"""
+
     def getIdentifiantGraphe(self):
         return self._identifiantGraphe
 
@@ -24,53 +26,48 @@ class Graphe:
     def getNumberNoeuds(self):
         return self._numberNoeuds
 
-
     """Les méthode"""
+
     def __str__(self):
         print("Noeud : " + str(self._dic_Noeuds) + "\n")
         print("Lien : " + str(self._dic_Liens) + "\n")
 
     def ajouterNoeud(self, noeud):
-        self._dic_Noeuds[noeud.getIdentifiantNoeud()]=noeud
-        self._numberNoeuds+=1
-
+        self._dic_Noeuds[noeud.getIdentifiantNoeud()] = noeud
+        self._numberNoeuds += 1
 
     def ajouterLien(self, lien):
-        self._dic_Liens[lien.getIdentifiantLien()]=lien
+        self._dic_Liens[lien.getIdentifiantLien()] = lien
 
     def obtenirProchainsNoeuds(self, identifiantNoeud):
-        identifiantNoeud=str(identifiantNoeud)
-        dic_prochainNoeud={}
+        identifiantNoeud = str(identifiantNoeud)
+        dic_prochainNoeud = {}
 
         for element in self._dic_Liens:
-            if element[0]  in identifiantNoeud:
-                dic_prochainNoeud[element]=self._dic_Liens[element].getNoeud2()
+            element1 = element
+            element = element.split("--")
+            if element[0] == identifiantNoeud:
+                dic_prochainNoeud[element1] = self._dic_Liens[element1].getNoeud2()
+            if element[1] == identifiantNoeud:
+                dic_prochainNoeud[element1] = self._dic_Liens[element1].getNoeud1()
 
         return dic_prochainNoeud
 
+    def obtenirProchainsNoeuds2(self, identifiantNoeud):
+        dic_prochainNoeud = {}
 
+        listLiens = self._dic_Liens  # Utilisation du dico Lien pour trouver les prochain Neoud
+        compteur = 0
 
-"""noeud1 = Noeud()
-noeud2 = Noeud()
-noeud3 = Noeud()
+        for element in listLiens:
+            print("Element " + element)
+            print("idNoeud " + str(identifiantNoeud))
+            if identifiantNoeud == element.getNoeud2().getIdentifiantNoeud():  #
 
+                idNoeudA = self._dic_Liens[element].getNoeud2()
+                dic_prochainNoeud[compteur] = idNoeudA
+                compteur += 1
 
-lien1 = Lien(noeud1.getIdentifiantNoeud(),noeud2.getIdentifiantNoeud(), 10)
-lien2 = Lien(noeud1.getIdentifiantNoeud(),noeud3.getIdentifiantNoeud(), 11)
-lien1.__str__()
-
-g1 = Graphe(10)
-g1.ajouterNoeud(noeud1)
-g1.ajouterNoeud(noeud2)
-g1.ajouterNoeud(noeud3)
-g1.ajouterLien(lien1)
-g1.ajouterLien(lien2)
-g1.__str__()
-
-print("Prochaine Noeud est :"+str(g1.obtenirProchainsNoeuds(noeud1.getIdentifiantNoeud())))
-
-
-print("ICI")"""
-
+        return dic_prochainNoeud
 
 
